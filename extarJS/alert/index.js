58304f816048d34ed69613f8c1d7ad6b122cb16a
akshay_ashok <akshayashokdj@gmail.com>
2022-09-10 22:09:53 +0530

    feat:if statement alert

---
+++ extarJS/alert/index.js
@@ -0,0 +1,11 @@
+let a = prompt("Enter a number1:");
+let b = prompt("Enter a number2:");
+
+function Greater(){
+    if(a>b){
+        alert("number1 is greater than number2.");
+    }
+    else{
+        alert("number2 is greater that number1.");
+    }
+}
